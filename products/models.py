7c6e297fda2f9836c54c2ae758ad6e80f3fc454f
Junth Basnet <junthbasnet@gmail.com>
2021-05-14 15:52:10 +0545

    :construction: subcategory model design

--- products/models.py
+++ products/models.py
@@ -33,6 +33,31 @@ class Category(SEOBaseModel):
         super().save(*args, **kwargs)
 
     
+class SubCategory(SEOBaseModel):
+    category = models.ForeignKey(
+        'Category',
+        on_delete=models.PROTECT,
+        related_name='sub_categories'
+    )
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    description = models.TextField(default='')
+    image = models.ImageField(upload_to='sub-categories',null=True, blank=True)
+
+    class Meta:
+        verbose_name = _('Sub-Category')
+        verbose_name_plural = _('Sub-Categories')
+    
+    def __str__(self):
+        return f'{self.name}'
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+
+
 
     
 
