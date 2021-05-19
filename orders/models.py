fa4e53ac19d1a2fc8b8a78f6115d2a52b4489ab1
Junth Basnet <junthbasnet@gmail.com>
2021-05-19 15:59:10 +0545

    :bookmark: promocode model design

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,26 @@
+from django.core.validators import MinValueValidator
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from common.models import TimeStampedModel
+
+
+class PromoCode(TimeStampedModel):
+    """
+    Model to store promo codes.
+    """
+    name = models.CharField(_('name'), max_length=127)
+    code = models.CharField(_('code'), max_length=127, unique=True)
+    discount = models.PositiveIntegerField(_('discount'), validators=[MinValueValidator(1)])
+    start_date = models.DateField(_('start date'))
+    end_date = models.DateField(_('end date'))
+
+    class Meta:
+        verbose_name = _('Coupon')
+        verbose_name_plural = _('Coupons')
+        ordering = ('-start_date',)
+
+    @property
+    def is_valid(self):
+        return self.start_date <= timezone.now().date() <= self.end_date
